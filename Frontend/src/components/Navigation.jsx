18130d048c125d237bbdfcc3be7d686e0489f4dd
SAMIR DHORAN <samirdhoran2000@gmail.com>
2023-07-20 18:42:58 +0530

    modified navbar

--- Frontend/src/components/Navigation.jsx
+++ Frontend/src/components/Navigation.jsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
 import "./navigation.css";
+const NAVIGATION = ["home", "explore", "contact us", "about us"];
 const Navigation = () => {
-  const NAVIGATION = ["Home", "Explore", "Contact Us", "About Us"];
+
+    const [nav, setNav] = useState('');
+
+    const onSelectNav = (element) => {
+        console.log(element);
+
+      setNav(element);
+      console.log(nav);
+    };
   return (
     <div>
       <ul className="navbar">
         {NAVIGATION.map((element) => {
-          return <li key={element}>{element}</li>;
+            return (
+              <li key={element} onClick={() => {onSelectNav(element);}} className="capitalize">
+                {element}
+              </li>
+            );
         })}
       </ul>
     </div>
