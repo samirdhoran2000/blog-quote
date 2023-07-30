b1e7e1c274112784463538414c8341bac7050cdc
SAMIR DHORAN <samirdhoran2000@gmail.com>
2023-07-30 15:13:55 +0530

    scroll bar css and quote data is chage

--- Frontend/src/Constant/quoteCardData.js
+++ Frontend/src/Constant/quoteCardData.js
@@ -1,51 +1,77 @@
 const Quotes = [
   {
     id: 1,
-    author: " peter",
+    author: "Franklin D. Roosevelt",
     content:
-      "May be not today, May be not tommarow, May be not in the next month, but one thing is true, I'll be success one day",
-    // content:
-    //   "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio m klsj vero nostrum.",
+      "The only limit to our realization of tomorrow will be our doubts of today.",
     views: 1,
     likes: 91,
   },
   {
     id: 2,
-    author: "sam peter",
-    content:
-      "May be not today, May be not tommarow, May be not in the next month, but one thing is true, I'll be success one day",
-    // content:
-    //   "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio m klsj vero nostrum.",
+    author: "Steve Jobs",
+    content: "Your time is limited, don't waste it living someone else's life.",
     views: 201,
     likes: 51,
   },
   {
     id: 3,
-    author: "sam ",
+    author: "Winston Churchill ",
     content:
-      "May be not today, May be not tommarow, May be not in the next month, but one thing is true, I'll be success one day",
-    // content:
-    //   "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio m klsj vero nostrum.",
+      "Success is not final, failure is not fatal: It is the courage to continue that counts.",
     views: 546,
     likes: 63,
   },
   {
     id: 4,
-    author: "eric garseti",
-    content:
-      "Time and tide stopes for none",
-    // content:
-    //   "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio m klsj vero nostrum.",
+    author: "Peter Drucker",
+    content: "The best way to predict the future is to create it.",
     views: 77,
     likes: 9,
   },
   {
     id: 5,
+    author: "Martin Luther King Jr.",
+    content:
+      "In the end, we will remember not the words of our enemies, but the silence of our friends.",
+    views: 33,
+    likes: 5,
+  },
+  {
+    id: 6,
+    author: " Ralph Waldo Emerson",
+    content:
+      "The only person you are destined to become is the person you decide to be.",
+    views: 1,
+    likes: 91,
+  },
+  {
+    id: 7,
+    author: "Nelson Mandela",
+    content:
+      "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+    views: 201,
+    likes: 51,
+  },
+  {
+    id: 8,
+    author: "Steve Jobs",
+    content: "The only way to do great work is to love what you do.",
+    views: 546,
+    likes: 63,
+  },
+  {
+    id: 9,
+    author: "Charles R. Swindoll",
+    content: "Life is 10% what happens to us and 90% how we react to it.",
+    views: 77,
+    likes: 9,
+  },
+  {
+    id: 10,
     author: "sam peter",
     content:
       "May be not today, May be not tommarow, May be not in the next month, but one thing is true, I'll be success one day",
-    // content:
-    //   "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio m klsj vero nostrum.",
     views: 33,
     likes: 5,
   },
--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -1,8 +1,72 @@
 import './footer.css'
 const Footer = () => {
   return (
-    <div className='footer-main-container'>Footer</div>
-  )
+    <div className="footer-main-container">
+      <div className="foot">
+        <h2
+          style={{
+            fontSize: "26px",
+          }}
+        >
+          Inspiration for Every Day
+        </h2>
+        <p
+          style={{
+            fontSize: "16px",
+          }}
+        >
+          Welcome to our vibrant world of quotes, where inspiration knows no
+          bounds. We strive to fuel your days with thought-provoking,
+          motivational, and heartwarming quotes from renowned thinkers, writers,
+          and personalities. Whether you seek words to lift your spirits,
+          broaden your perspective, or simply to share with loved ones, our
+          extensive collection will undoubtedly resonate with your soul. 
+        </p>
+      </div>
+      <div className="foot">
+        <h2
+          style={{
+            fontSize: "26px",
+          }}
+        >
+          Community of Quote Enthusiasts
+        </h2>
+        <p
+          style={{
+            fontSize: "16px",
+          }}
+        >
+          At Blog Quote, we believe in the strength of community. Our platform
+          brings together quote enthusiasts from every corner of the globe,
+          fostering a welcoming space to engage, learn, and grow together. Share
+          your favorite quotes, engage in discussions, and connect with
+          like-minded individuals who appreciate the profound impact of words.
+          
+        </p>
+      </div>
+      <div className="foot">
+        <h2
+          style={{
+            fontSize: "26px",
+          }}
+        >
+          Words that Empower, Words that Connect
+        </h2>
+        <p
+          style={{
+            fontSize: "16px",
+          }}
+        >
+          In a world filled with constant chatter, we curate and present quotes
+          that have the power to empower and connect. Our handpicked selection
+          reflects the diversity of human experiences, addressing themes of
+          love, resilience, success, and more. Each quote is a timeless reminder
+          of the strength within you, urging you to embrace your journey with
+          renewed determination. 
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Footer
